7bed8eabb9fb1fdc1afacc8cd09beab685529e2c
MR-KIMDAUN <people5474@naver.com>
2022-03-23 20:37:34 +0900

    20220323

---
+++ src/main/java/test.java
@@ -0,0 +1,13 @@
+public class test {
+    public static void main(String[] args) {
+        int a;
+        double b;
+
+        a = 100;
+        b = 123.33;
+
+        System.out.printf("%d\n", a);
+        System.out.printf("%f\n", b);
+    }
+}
+
